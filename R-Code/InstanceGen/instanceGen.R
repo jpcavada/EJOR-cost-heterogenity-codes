155425050eb04b6ea6d3319bad229cbd485fc8ea
Juan Ignacio Zambrano <juano@Juans-MacBook-Air.local>
2019-03-12 13:31:14 -0300

    ins3 e ins4 agregadas

--- R-Code/InstanceGen/instanceGen.R
+++ R-Code/InstanceGen/instanceGen.R
@@ -44,9 +44,9 @@ write.csv(baseInst100, file = "baseInst100_2.csv")
 
 ######
 #Escenario 2.
-#Costo por distancia $3.000 / ($50 /min)
-#Costo por Atraso: $3.600  / ($60 / min)
-#Penalty Promedio:  
+#Costo por distancia $3.300 / ($50 /min)
+#Costo por Atraso: $4.800  / ($60 / min)
+#Penalty Promedio: 21.600 (Corriendo JP)
 ######
 
 custList <- aggregate(baseInst100$razonSocial, by=list(baseInst100$razonSocial), FUN = length )
@@ -71,7 +71,53 @@ write.csv(ins2, file="~/Gitlab/xerox-Paper/Data/ins2.csv")
 
 ######
 #Escenario 3.
-#Costo por distancia $3.000 / ($50 /min)
-#Costo por Atraso: $3.000  / ($50 / min)
-#Penalty Promedio:  
+#Costo por distancia $3.300 / ($50 /min)
+#Costo por Atraso: $4.800  / ($50 / min)
+#Penalty Promedio:  32.400 
 ######
+
+custList <- aggregate(baseInst100$razonSocial, by=list(baseInst100$razonSocial), FUN = length )
+
+N <- 249
+mu <- 32400
+
+p <- data.frame(rep(mu, N))
+p05 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.05)))
+p10 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.1)))
+p20 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.2)))
+p25 <- data.frame(sort(abs(rnorm(N, mean = mu, sd = mu*0.25))))
+p15 <- data.frame(sort(abs(rnorm(N, mean = mu, sd = mu*0.15))))
+p50 <- data.frame(sort(abs(rnorm(N, mean = mu, sd =mu*0.50))))
+p100 <- data.frame(sort(abs(rnorm(N, mean = mu, sd =mu))))
+
+custData <- cbind(custList[,1], p, p05,p10, p15, p20, p25, p50,p100)
+colnames(custData) <- c('razonSocial','p', 'p05', 'p10', 'p15', 'p20', 'p25', 'p50', 'p100')
+
+ins3 <-merge(baseInst100,custData, by='razonSocial')
+write.csv(ins3, file="~/Gitlab/xerox-Paper/Data/ins3.csv")
+
+######
+#Escenario 4.
+#Costo por distancia $3.300 / ($50 /min)
+#Costo por Atraso: $4.800  / ($50 / min)
+#Penalty Promedio:  10.800 
+######
+
+N <- 249
+mu <- 10800
+
+p <- data.frame(rep(mu, N))
+p05 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.05)))
+p10 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.1)))
+p20 <- data.frame(sort(rnorm(N, mean = mu, sd = mu*0.2)))
+p25 <- data.frame(sort(abs(rnorm(N, mean = mu, sd = mu*0.25))))
+p15 <- data.frame(sort(abs(rnorm(N, mean = mu, sd = mu*0.15))))
+p50 <- data.frame(sort(abs(rnorm(N, mean = mu, sd =mu*0.50))))
+p100 <- data.frame(sort(abs(rnorm(N, mean = mu, sd =mu))))
+
+custData <- cbind(custList[,1], p, p05,p10, p15, p20, p25, p50,p100)
+colnames(custData) <- c('razonSocial','p', 'p05', 'p10', 'p15', 'p20', 'p25', 'p50', 'p100')
+
+ins4 <-merge(baseInst100,custData, by='razonSocial')
+write.csv(ins4, file="~/Gitlab/xerox-Paper/Data/ins4.csv")
+
